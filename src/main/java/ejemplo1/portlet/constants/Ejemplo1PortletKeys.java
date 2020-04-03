3be3dfbee2154943f7b79514c85c3999b389d8ec
macbook <macbook@MacBook-Pro-de-macbook.local>
2020-04-02 20:59:10 -0500

    First commit

---
+++ src/main/java/ejemplo1/portlet/constants/Ejemplo1PortletKeys.java
@@ -0,0 +1,11 @@
+package ejemplo1.portlet.constants;
+
+/**
+ * @author macbook
+ */
+public class Ejemplo1PortletKeys {
+
+	public static final String EJEMPLO1 =
+		"ejemplo1_portlet_Ejemplo1Portlet";
+
+}
---
+++ src/main/java/ejemplo1/portlet/portlet/Ejemplo1Portlet.java
@@ -0,0 +1,30 @@
+package ejemplo1.portlet.portlet;
+
+import ejemplo1.portlet.constants.Ejemplo1PortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author macbook
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Ejemplo1",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + Ejemplo1PortletKeys.EJEMPLO1,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class Ejemplo1Portlet extends MVCPortlet {
+}
